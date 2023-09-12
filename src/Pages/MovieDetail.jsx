8b9125b02c53249319f864352d403a9fe6e3541f
Nosa10 <edisonnosakhare@gmail.com>
2023-09-11 19:57:25 -0700

    New push

--- src/Pages/MovieDetail.jsx
+++ src/Pages/MovieDetail.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useState, useEffect } from "react";
-import './MovieDetails.css'
+import './Moviedetails.css'
 import { useParams } from "react-router-dom";
 import axios from "axios";
 
